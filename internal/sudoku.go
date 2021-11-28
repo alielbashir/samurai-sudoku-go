213555edbcd4b05597faa21ac2f672e99bf2a95f
Ali Elbashir <alielbashiir@gmail.com>
2021-11-28 06:43:07 +0300

    Add sub-sudoku getters and tests

---
+++ internal/sudoku.go
@@ -0,0 +1,176 @@
+package internal
+
+import (
+	"bytes"
+	"fmt"
+	"sync"
+)
+
+type Matrix [][]int
+
+type SamuraiSudoku struct {
+	mu   sync.Mutex
+	grid Matrix
+}
+
+func (s *SamuraiSudoku) Grid() Matrix {
+	return s.grid
+}
+
+func (s *SamuraiSudoku) SetGrid(grid Matrix) {
+	s.grid = grid
+}
+
+type Position int
+
+const (
+	TopLeft Position = iota
+	TopRight
+	Centre
+	BottomLeft
+	BottomRight
+)
+
+func (p Position) String() string {
+	switch p {
+	case TopLeft:
+		return "top left"
+	case TopRight:
+		return "top right"
+	case Centre:
+		return "centre"
+	case BottomLeft:
+		return "bottom left"
+	case BottomRight:
+		return "bottom right"
+	}
+	return "unknown"
+}
+
+func (m Matrix) PrintSudoku() string {
+	var buf bytes.Buffer
+	var char string
+	for _, row := range m {
+		for _, num := range row {
+			if num == -1 {
+				char = ""
+			} else {
+				char = string(rune(num))
+			}
+			_, err := fmt.Fprint(&buf, char, " ")
+			if err != nil {
+				return ""
+			}
+		}
+		_, err := fmt.Fprint(&buf, "\n")
+		if err != nil {
+			return ""
+		}
+	}
+	return buf.String()
+}
+
+// Returns sub-sudoku for given position, assuming 21*21 samurai sudoku grid
+func (s *SamuraiSudoku) GetSubSudoku(position Position) [][]int {
+	var grid = s.grid
+	var subSudoku [][]int
+	switch position {
+	case TopLeft:
+		subSudoku = grid[0:9]
+		for i := range subSudoku {
+			subSudoku[i] = subSudoku[i][0:9]
+		}
+	case TopRight:
+		subSudoku = grid[0:9]
+		for i := range subSudoku {
+			subSudoku[i] = subSudoku[i][12:21]
+		}
+	case Centre:
+		subSudoku = grid[6:15]
+		for i := range subSudoku {
+			subSudoku[i] = subSudoku[i][6:15]
+		}
+
+	case BottomLeft:
+		subSudoku = grid[12:21]
+		for i := range subSudoku {
+			subSudoku[i] = subSudoku[i][0:9]
+		}
+
+	case BottomRight:
+		subSudoku = grid[12:21]
+		for i := range subSudoku {
+			subSudoku[i] = subSudoku[i][12:21]
+		}
+	}
+	return subSudoku
+}
+
+func SudokuString(sudoku [][]int) string {
+	var buf bytes.Buffer
+	for _, row := range sudoku {
+		for _, num := range row {
+			_, err := fmt.Fprint(&buf, num, " ")
+			if err != nil {
+				return ""
+			}
+		}
+		_, err := fmt.Fprint(&buf, "\n")
+		if err != nil {
+			return ""
+		}
+	}
+	return buf.String()
+}
+
+func possible(sudoku [][]int, y int, x int, n int) bool {
+	for i := 0; i < 9; i++ {
+		if sudoku[y][i] == n {
+			return false
+		}
+	}
+	for i := 0; i < 9; i++ {
+		if sudoku[i][x] == n {
+			return false
+		}
+	}
+	x0 := (x / 3) * 3
+	y0 := (y / 3) * 3
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if sudoku[y0+i][x0+j] == n {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func SolveSudoku(sudoku [][]int) [][]int {
+	backtrack(sudoku)
+	return sudoku
+}
+
+func backtrack(sudoku [][]int) bool {
+	for y := 0; y < 9; y++ {
+		for x := 0; x < 9; x++ {
+			// if cell is empty
+			if sudoku[y][x] == 0 {
+				for n := 1; n < 10; n++ {
+					if possible(sudoku, y, x, n) {
+						sudoku[y][x] = n
+						if backtrack(sudoku) {
+							return true
+						}
+						sudoku[y][x] = 0
+					}
+				}
+				return false
+			}
+		}
+	}
+	//fmt.Println("Solved!")
+	//PrintSudoku(sudoku)
+	return true
+}
--- main.go
+++ main.go
@@ -1,81 +1,6 @@
 package main
 
-import "fmt"
-
-func printSudoku(sudoku [][]int) {
-	fmt.Println()
-	for _, row := range sudoku {
-		for _, num := range row {
-			fmt.Print(num, " ")
-		}
-		fmt.Println()
-	}
-}
-
-//func printSamuraiSudoku(samurai [][]int) {
-//	fmt.Println()
-//	for _, row := range samurai {
-//		for _, num := range row {
-//			if num == -1 {
-//				fmt.Print("  ")
-//			} else {
-//				fmt.Print(num, " ")
-//			}
-//		}
-//		fmt.Println()
-//	}
-//}
-
-func possible(sudoku [][]int, y int, x int, n int) bool {
-	for i := 0; i < 9; i++ {
-		if sudoku[y][i] == n {
-			return false
-		}
-	}
-	for i := 0; i < 9; i++ {
-		if sudoku[i][x] == n {
-			return false
-		}
-	}
-	x0 := (x / 3) * 3
-	y0 := (y / 3) * 3
-
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			if sudoku[y0+i][x0+j] == n {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-//func solveSudoku(bigSudoku [][]int) {
-//
-//}
-
-func backtrack(sudoku [][]int) bool {
-	for y := 0; y < 9; y++ {
-		for x := 0; x < 9; x++ {
-			// if cell is empty
-			if sudoku[y][x] == 0 {
-				for n := 1; n < 10; n++ {
-					if possible(sudoku, y, x, n) {
-						sudoku[y][x] = n
-						if backtrack(sudoku) {
-							return true
-						}
-						sudoku[y][x] = 0
-					}
-				}
-				return false
-			}
-		}
-	}
-	fmt.Println("Solved!")
-	printSudoku(sudoku)
-	return true
-}
+import . "samurai-sudoku/internal"
 
 func main() {
 	var sudoku = [][]int{
@@ -89,7 +14,5 @@ func main() {
 		{0, 0, 0, 4, 1, 9, 0, 0, 5},
 		{0, 0, 0, 0, 8, 0, 0, 0, 0},
 	}
-	printSudoku(sudoku)
-	backtrack(sudoku)
-	//printSudoku()
+	SolveSudoku(sudoku)
 }
